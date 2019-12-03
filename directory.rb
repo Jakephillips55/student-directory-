7e324744120e0d8a3f8801985bf88813d6a3c346
Jake <Jakep629@gmail.com>
2019-12-03 18:07:03 +0000

    ex2

--- directory.rb
+++ directory.rb
@@ -3,20 +3,20 @@ def input_students
   puts "To finish, just hit return twice"
   students = []
   name = gets.chomp
-  while !name.empty? do
+  while !name.empty? and name[0] == "f" || name[0] == "F" do
     students << {name: name, cohort: :november}
     puts "Now we have #{students.count} students"
     name = gets.chomp
   end
   students
-end 
+end
 def print_header
 puts "The students of Villains Academy"
 puts "-------------"
 end
 def print(students)
-  students.each do |student|
-    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+  students.each_with_index do |student, index|
+    puts "#{index}. #{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
 def print_footer(students)
