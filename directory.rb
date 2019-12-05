8d22635e3315a776dd1587ca1493a1549bd6269c
Jake <Jakep629@gmail.com>
2019-12-05 15:22:42 +0000

    first steps of interactive menu

--- directory.rb
+++ directory.rb
@@ -1,28 +1,61 @@
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
-  students = []
+  print "name: "
+  @students = []
   name = gets.chomp
-  while !name.empty? and name[0] == "f" || name[0] == "F" do
-    students << {name: name, cohort: :november}
-    puts "Now we have #{students.count} students"
+  while !name.empty?
+    @students << {name: name, cohort: :november}
+    puts "Now we have #{@students.count} students"
     name = gets.chomp
   end
-  students
+  @students
 end
+
 def print_header
 puts "The students of Villains Academy"
 puts "-------------"
 end
-def print(students)
-  students.each_with_index do |student, index|
-    puts "#{index}. #{student[:name]} (#{student[:cohort]} cohort)"
+def mprint(students)
+@students.each do |student, index|
+    puts "#{index}. #{@student[:name]} (#{@student[:cohort]} cohort)"
   end
 end
 def print_footer(students)
-  puts "Overall, we have #{students.count} great students"
+  puts "Overall, we have #{@students.count} great students"
 end
+
+def interactive_menu
+  @students = []
+  loop do
+    selection = gets.chomp
+    case selection
+    when "1"
+      @students = input_students
+    when "2"
+    show_students
+    when "9"
+      exit
+    else
+      puts "I dont know what that meant, try again"
+    end
+  end
+end
+
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit"
+end
+
+def show_students
+  print_header
+  print(@students)
+  print_footer(@students)
+end
+print_menu
+interactive_menu
 students = input_students
 print_header
-print(students)
-print_footer(students)
+mprint(@students)
+print_footer(@students)
