0d317dd29cefd9d3166a1e7505dc2637d1c100de
Jake <Jakep629@gmail.com>
2019-12-05 17:18:06 +0000

    save data cmd

--- directory.rb
+++ directory.rb
@@ -1,61 +1,74 @@
+@students = []
+
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
-  print "name: "
-  @students = []
   name = gets.chomp
-  while !name.empty?
-    @students << {name: name, cohort: :november}
+  while !name.empty? do
+      @students << {name: name, cohort: :november}
     puts "Now we have #{@students.count} students"
-    name = gets.chomp
+      name = gets.chomp
   end
-  @students
-end
-
-def print_header
-puts "The students of Villains Academy"
-puts "-------------"
-end
-def mprint(students)
-@students.each do |student, index|
-    puts "#{index}. #{@student[:name]} (#{@student[:cohort]} cohort)"
-  end
-end
-def print_footer(students)
-  puts "Overall, we have #{@students.count} great students"
 end
 
 def interactive_menu
-  @students = []
   loop do
-    selection = gets.chomp
-    case selection
-    when "1"
-      @students = input_students
-    when "2"
-    show_students
-    when "9"
-      exit
-    else
-      puts "I dont know what that meant, try again"
-    end
+    print_menu
+    process(gets.chomp)
   end
 end
 
 def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
+  puts "3. Save the list to students.csv"
   puts "9. Exit"
 end
 
 def show_students
   print_header
-  print(@students)
-  print_footer(@students)
+  print_student_list
+  print_footer
 end
-print_menu
+
+def process(selection)
+  case selection
+  when "1"
+    input_students
+  when "2"
+    show_students
+  when "3"
+    save_students 
+  when "9"
+    exit
+  else
+    puts "I don't know what you meant, try again"
+  end
+end
+
+def print_header
+  puts "The students of Villains Academy"
+  puts "-------------"
+end
+
+def print_student_list
+  @students.each do |student|
+    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+  end
+end
+
+def print_footer
+  puts "Overall, we have #{@students.count} great students"
+end
+
+def save_students
+  file = File.open("students.csv", "w")
+  @students.each { |student|
+    student_data = [student[:name], student[:cohort]]
+    csv_line = student_data.join(",")
+    file.puts csv_line
+  }
+  file.close
+end
+
 interactive_menu
-students = input_students
-print_header
-mprint(@students)
-print_footer(@students)
