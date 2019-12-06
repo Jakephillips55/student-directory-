3b7ca4dd55b3f605bf5e4169d8f579538ee5abc4
Jake <Jakep629@gmail.com>
2019-12-06 17:16:57 +0000

    fully functional app

--- directory.rb
+++ directory.rb
@@ -3,18 +3,18 @@
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
-  name = gets.chomp
+  name = STDIN.gets.chomp
   while !name.empty? do
       @students << {name: name, cohort: :november}
     puts "Now we have #{@students.count} students"
-      name = gets.chomp
+      name = STDIN.gets.chomp
   end
 end
 
 def interactive_menu
   loop do
     print_menu
-    process(gets.chomp)
+    process(STDIN.gets.chomp)
   end
 end
 
@@ -22,6 +22,7 @@ def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
   puts "3. Save the list to students.csv"
+  puts "4. Load the list of students from students.csv"
   puts "9. Exit"
 end
 
@@ -39,6 +40,8 @@ def process(selection)
     show_students
   when "3"
     save_students
+  when "4"
+    load_students
   when "9"
     exit
   else
@@ -69,7 +72,27 @@ def save_students
     file.puts csv_line
   }
   file.close
-
 end
 
+def load_students(filename = "students.csv")
+  file = File.open(filename, "r")
+  file.readlines.each { |line|
+  name, cohort = line.chomp.split(',')
+    @students << {name: name, cohort: cohort.to_sym}
+  }
+  file.close
+end
+
+def try_load_students
+  filename = ARGV.first
+  return if filename.nil?
+  if File.exists?(filename)
+    load_students(filename)
+    puts "Loaded #{@students.count} from #{filename}"
+  else
+    puts "Sorry #{filename} dosen't exist."
+    exit
+  end
+end
+load_students
 interactive_menu
