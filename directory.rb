b3e039b217d20b999b5e64f4ca58b68708101713
Jake <Jakep629@gmail.com>
2019-12-08 16:04:08 +0000

    dont repeat

--- directory.rb
+++ directory.rb
@@ -5,10 +5,14 @@ def input_students
   puts "To finish, just hit return twice"
   name = STDIN.gets.chomp
   while !name.empty? do
-      @students << {name: name, cohort: :november}
+    add_to_students(name)
     puts "Now we have #{@students.count} students"
       name = STDIN.gets.chomp
   end
+end
+
+def add_to_students(name)
+  @students << {name: name, cohort: :november}
 end
 
 def interactive_menu
@@ -79,7 +83,7 @@ def load_students(filename = "students.csv")
   file = File.open(filename, "r")
   file.readlines.each { |line|
   name, cohort = line.chomp.split(',')
-    @students << {name: name, cohort: cohort.to_sym}
+  add_to_students(name)
   }
   file.close
 end
